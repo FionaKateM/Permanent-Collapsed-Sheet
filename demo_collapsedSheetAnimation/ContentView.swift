a881d7ffccc71571d49f14881ba366bad7474690
Fiona Kate <fkmorgan@me.com>
2024-07-03 16:12:13 +0100

    Initial Commit

---
+++ demo_collapsedSheetAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo_collapsedSheetAnimation
+//
+//  Created by Fiona Kate Morgan on 03/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo_collapsedSheetAnimation/demo_collapsedSheetAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_collapsedSheetAnimationApp.swift
+//  demo_collapsedSheetAnimation
+//
+//  Created by Fiona Kate Morgan on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct demo_collapsedSheetAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
